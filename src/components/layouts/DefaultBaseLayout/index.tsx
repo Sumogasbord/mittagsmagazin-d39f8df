2e49e7bd2a1924bb5c6be9e3d5d544840b3bb578
Sumögåsbord <sumogasbord@gmail.com>
2024-07-30 10:37:47 +0900

    Plainbox Font setting

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -17,6 +17,19 @@ export default function DefaultBaseLayout(props) {
             <div className="sb-base sb-default-base-layout">
                 <Head>
                     <title>{title}</title>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        (function(d) {
+                            var config = {
+                            kitId: 'csb3exr',
+                            scriptTimeout: 3000,
+                            async: true
+                            },
+                            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+                        })(document);
+                            `}}
+                    />
                     {metaDescription && <meta name="description" content={metaDescription} />}
                     {metaTags.map((metaTag) => {
                         if (metaTag.format === 'property') {
@@ -27,21 +40,6 @@ export default function DefaultBaseLayout(props) {
                     })}
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     {site.favicon && <link rel="icon" href={site.favicon} />}
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                        <script>
-                        (function(d) {
-                            var config = {
-                            kitId: 'csb3exr',
-                            scriptTimeout: 3000,
-                            async: true
-                            },
-                            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-                        })(document);
-                        </script>
-                            `}}
-                    />
                 </Head>
                 {site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
                 {props.children}
