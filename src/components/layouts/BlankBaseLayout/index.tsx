31fe9ac114cf46c566cde831a5541d3bd4c7e1b6
Sumögåsbord <sumogasbord@gmail.com>
2024-07-30 10:33:16 +0900

    Plainbox Font settings

--- src/components/layouts/BlankBaseLayout/index.tsx
+++ src/components/layouts/BlankBaseLayout/index.tsx
@@ -10,16 +10,21 @@ export default function BlankBaseLayout(props) {
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
             <Head>
                 <title>{page.title}</title>
-                <script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    <script>
                     (function(d) {
-                    var config = {
-                        kitId: 'sgl7wec',
-                    scriptTimeout: 3000,
-                    async: true
-                    },
-                    h=d.documentElement,t=setTimeout(function(){h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a = this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){ }};s.parentNode.insertBefore(tk,s)
-                })(document);
-                </script>
+                        var config = {
+                        kitId: 'csb3exr',
+                        scriptTimeout: 3000,
+                        async: true
+                        },
+                        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+                    })(document);
+                    </script>
+                        `}}
+                />
                 <meta name="description" content="Components Library" />
                 {site.favicon && <link rel="icon" href={site.favicon} />}
             </Head>
