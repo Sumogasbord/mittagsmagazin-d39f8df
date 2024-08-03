4a784d13abc7db5774ccc61ab4fbbbdaf218cd61
Sumögåsbord <sumogasbord@gmail.com>
2024-08-03 12:45:34 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
                 primary: themeStyle.primary
             },
             fontFamily: {
-                sans: ['neue-haas-unica', 'zen-kaku-gothic-new','sans-serif'],
+                sans: ['new-rubrik-edge', 'm-plus-2p','sans-serif'],
                 serif: ['antonia-variable', 'dnp-shuei-mincho-pr6', 'serif']
             },
             gridTemplateColumns: {
