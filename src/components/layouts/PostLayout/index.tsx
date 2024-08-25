6261231a80ea8639120f59bbe951d0af9cd32f6d
Sumögåsbord <sumogasbord@gmail.com>
2024-08-26 01:26:42 +0900

    あきらめた

--- src/components/layouts/PostLayout/index.tsx
+++ src/components/layouts/PostLayout/index.tsx
@@ -1,44 +1,22 @@
 import * as React from 'react';
-import classNames from 'classnames';
 import dayjs from 'dayjs';
 import Markdown from 'markdown-to-jsx';
 
 import { getBaseLayoutComponent } from '../../../utils/base-layout';
 import { getComponent } from '../../components-registry';
 import Link from '../../atoms/Link';
-import ImageBlock from '../../blocks/ImageBlock';
 
 export default function PostLayout(props) {
-    const { page, showThumbnail, site, hasBigThumbnail, hasAnnotations } = props;
+    const { page, site } = props;
     const BaseLayout = getBaseLayoutComponent(page.baseLayout, site.baseLayout);
     const { enableAnnotations = true } = site;
     const { title, date, author, markdown_content, bottomSections = [] } = page;
     const dateTimeAttr = dayjs(date).format('YYYY-MM-DD HH:mm:ss');
     const formattedDate = dayjs(date).format('MMMM D, YYYY');
-    const flexDirection = page.styles?.self?.flexDirection ?? 'col';
-    const hasThumbnail = !!(showThumbnail && page.featuredImage?.url);
 
     return (
         <BaseLayout page={page} site={site}>
             <main id="main" className="sb-layout sb-post-layout">
-                <div className={classNames('w-full', 'flex', mapFlexDirectionStyles(flexDirection, hasThumbnail), 'gap-6')}>
-                    {hasThumbnail && (
-                        <ImageBlock
-                            {...page.featuredImage}
-                            className={classNames({
-                                'xs:w-[50%] xs:shrink-0': hasBigThumbnail && (flexDirection === 'row' || flexDirection === 'row-reversed'),
-                                'xs:w-[28.4%] xs:shrink-0': !hasBigThumbnail && (flexDirection === 'row' || flexDirection === 'row-reversed')
-                            })}
-                            imageClassName="w-full h-full object-cover"
-                            {...(hasAnnotations && { 'data-sb-field-path': 'featuredImage' })}
-                        />
-                    )}
-                    <div
-                        className={classNames('w-full', {
-                            'xs:grow': hasThumbnail && (flexDirection === 'row' || flexDirection === 'row-reversed')
-                        })}
-                    ></div>
-                </div>
                 <article className="px-4 py-16 sm:py-28">
                     <div className="max-w-screen-2xl mx-auto">
                         <header className="max-w-4xl mx-auto mb-12 text-center">
@@ -98,21 +76,6 @@ function PostAuthor({ author, enableAnnotations }) {
     ) : (
         <span {...(enableAnnotations && { 'data-sb-field-path': 'author' })}>{authorName}</span>
     );
-}
-
-function mapFlexDirectionStyles(flexDirection: string, hasThumbnail: boolean) {
-    switch (flexDirection) {
-        case 'row':
-            return hasThumbnail ? 'flex-col xs:flex-row xs:items-stretch' : 'flex-col';
-        case 'row-reverse':
-            return hasThumbnail ? 'flex-col xs:flex-row-reverse xs:items-stretch' : 'flex-col';
-        case 'col':
-            return 'flex-col';
-        case 'col-reverse':
-            return 'flex-col-reverse';
-        default:
-            return null;
-    }
 }
 
 /*
